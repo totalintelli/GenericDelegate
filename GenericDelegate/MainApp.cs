c60d38c4ae2a738fbe4ec041937ab63f3ebc6506
DEVZONEYDS\USER <kwanwoo5@naver.com>
2017-12-27 11:13:51 +0900

    first commit

---
+++ GenericDelegate/MainApp.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+델리게이트 사용 예제
+출처 : 박상현, 뇌를 자극하는 C# 4.0 프로그래밍, 389~391쪽
+*/
+
+namespace GenericDelegate
+{
+    delegate int Compare<T>(T a, T b);
+
+    class MainApp
+    {
+        static int AscendCompare<T>(T a, T b) where T : IComparable<T>
+        {
+            return a.CompareTo(b);
+        }
+
+        static int DescendCompare<T>(T a, T b) where T : IComparable<T>
+        {
+            return a.CompareTo(b) * -1;
+        }
+
+        static void BubbleSort<T>(T[] DataSet, Compare<T> Comparer)
+        {
+            int i = 0;
+            int j = 0;
+            T temp;
+
+            for (i = 0; i < DataSet.Length - 1; i ++)
+            {
+                for (j = 0; j < DataSet.Length - (i + 1); j++)
+                {
+                    if(Comparer(DataSet[j], DataSet[j + 1]) > 0)
+                    {
+                        temp = DataSet[j + 1];
+                        DataSet[j + 1] = DataSet[j];
+                        DataSet[j] = temp;
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 7, 4, 2, 10 };
+
+            Console.WriteLine("Sorting ascending...");
+            BubbleSort<int>(array, new Compare<int>(AscendCompare));
+
+            for (int i = 0; i < array.Length; i++)
+                Console.Write("{0} ", array[i]);
+
+            string[] array2 = { "abc", "def", "ghi", "jkl", "mno" };
+
+            Console.WriteLine("\nSorting descending...");
+            BubbleSort<string>(array2, new Compare<string>(DescendCompare));
+
+            for (int i = 0; i < array2.Length; i++)
+                Console.Write("{0} ", array2[i]);
+
+            Console.WriteLine();
+        }
+    }
+}
